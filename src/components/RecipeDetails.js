87d92bcfe4ddc340d8427ba267c5f11fe397e0fd
willianAD <willian.afonso013@gmail.com>
2022-12-02 14:27:51 -0300

    Requisito 26 finalizado

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -57,8 +57,24 @@ export default function RecipeDetails() {
     return array;
   };
 
+  const fetchRecommendationMeals = async () => {
+    const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+    const response = await fetch(URL);
+    const data = await response.json();
+    return data;
+  };
+
+  const fetchRecommendationDrinks = async () => {
+    const URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+    const response = await fetch(URL);
+    const data = await response.json();
+    return data;
+  };
+
   useEffect(() => {
     handleDispatch();
+    fetchRecommendationMeals();
+    fetchRecommendationDrinks();
   }, []);
 
   return (
