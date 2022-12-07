2de49f882602463b66f76610c1201f1f8758aef4
Vinicius Santiago <vinicius.santiago997@gmail.com>
2022-12-07 15:21:48 -0300

    Requisito 19 realizado

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -2,57 +2,69 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 export default function Recipes() {
-  const [recipes, setRecipes] = useState('');
-  const [isLoading, setLoading] = useState(true);
+  const [mealsRecipes, setMealsRecipes] = useState('');
+  const [drinksRecipes, setDrinksRecipes] = useState('');
   const history = useHistory();
-  useEffect(() => {
+
+  const fetchRecipes = async () => {
     const {
       location: { pathname },
     } = history;
-    const fetchRecipes = async () => {
-      try {
-        if (pathname === '/meals') {
-          const mealUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
-          const response = await fetch(mealUrl);
-          const data = await response.json();
-          const maxIndex = 12;
-          const result = data.meals.slice(0, maxIndex);
-          setRecipes(result);
-          console.log(result);
-          setLoading(false);
-        } else if (pathname === '/drinks') {
-          const drinkUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-          const response = await fetch(drinkUrl);
-          const data = await response.json();
-          const maxIndex = 12;
-          const result = data.drinks.slice(0, maxIndex);
-          setRecipes(result);
-          console.log(result);
-          setLoading(false);
-        }
-      } catch (error) {
-        console.log(error);
+    try {
+      if (pathname === '/meals') {
+        const mealUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+        const response = await fetch(mealUrl);
+        const data = await response.json();
+        const maxIndex = 12;
+        const result = data.meals.slice(0, maxIndex);
+        setMealsRecipes(result);
+      } else if (pathname === '/drinks') {
+        const drinkUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+        const response = await fetch(drinkUrl);
+        const data = await response.json();
+        const maxIndex = 12;
+        const result = data.drinks.slice(0, maxIndex);
+        setDrinksRecipes(result);
       }
-    };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
     fetchRecipes();
-    console.log(recipes);
-    console.log(isLoading);
   }, []);
-
-  console.log(recipes);
-  const {
-    location: { pathname },
-  } = history;
-
+  console.log(drinksRecipes);
   return (
     <>
-      { pathname === '/meals'
-        ? recipes.map((recipe, index) => (
-          <div key={ index }>{recipe.strMeal}</div>
+      {mealsRecipes ? (
+        mealsRecipes.map((recipe, index) => (
+          <div key={ index } data-testid={ `${index}-recipe-card` }>
+            <img
+              src={ recipe.strMealThumb }
+              alt={ `${recipe.strMeal} thumb` }
+              data-testid={ `${index}-card-img` }
+            />
+            <p data-testid={ `${index}-card-name` }>{recipe.strMeal}</p>
+          </div>
         ))
-        : recipes.map((recipe, index) => (
-          <div key={ index }>{recipe.strDrink}</div>
-        ))}
+      ) : (
+        <p />
+      )}
+      {drinksRecipes ? (
+        drinksRecipes.map((recipe, index) => (
+          <div key={ index } data-testid={ `${index}-recipe-card` }>
+            <img
+              src={ recipe.strDrinkThumb }
+              alt={ `${recipe.strDrink} thumb` }
+              data-testid={ `${index}-card-img` }
+            />
+            <p data-testid={ `${index}-card-name` }>{recipe.strDrink}</p>
+          </div>
+        ))
+      ) : (
+        <p />
+      )}
     </>
   );
 }
